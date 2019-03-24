ebf4a7f21cc4556c56afce34a3cd672d1da150dd
divdiff <mmaryon155@gmail.com>
2019-03-23 23:31:07 -0400

    Formatting code

--- src/main/java/com/mike/maryon/camel/web/scrapper/route/ScrapingRoute.java
+++ src/main/java/com/mike/maryon/camel/web/scrapper/route/ScrapingRoute.java
@@ -9,21 +9,12 @@ public class ScrapingRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
-//		BufferedReader br = new BufferedReader(new FileReader("src/main/resources/urls.txt"));
-//		String line = "";
-//		List<String> urls = new ArrayList<>();
-//		while ((line = br.readLine()) != null) {
-//			urls.add(line);
-//		}
-//		
-//		for (String url : urls) {
-		String url = "w3schools.com";
-			from("direct:startScrape")
-				.log("Before Rest Call to " + url)
-				.setHeader(Exchange.HTTP_METHOD, simple("GET"))
-				.setHeader("url", simple(url))
-				.to("http://" + url).process(new ScrapingProcessor())
-				.log("After Rest Call");
-//		}
+		String url = "camel.apache.org";
+		from("direct:startScrape")
+			.log("Before Rest Call to " + url)
+			.setHeader(Exchange.HTTP_METHOD, simple("GET"))
+			.setHeader("url", simple(url))
+			.to("http://" + url).process(new ScrapingProcessor())
+			.log("After Rest Call");
 	}
 }
