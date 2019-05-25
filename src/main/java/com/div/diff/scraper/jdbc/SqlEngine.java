8dd50d35342ee9edb8daa2e6ff4116c92ea3bc41
divdiff <mmaryon155@gmail.com>
2019-05-25 15:10:08 -0400

    Added SqlEngine

---
+++ src/main/java/com/div/diff/scraper/jdbc/SqlEngine.java
@@ -0,0 +1,18 @@
+package com.div.diff.scraper.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class SqlEngine {
+	
+	public Connection establishConn(String dbName, String user, String password) throws SQLException {
+		String url = "jdbc:postgresql://localhost/" + dbName;
+		Properties props = new Properties();
+		props.setProperty("user", user);
+		props.setProperty("password", password);
+		props.setProperty("ssl", "true");
+		return DriverManager.getConnection(url, props);
+	}
+}
