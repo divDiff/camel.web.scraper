50640ff7441cb08f0c620d9ea5b2f8db8260f7be
divdiff <mmaryon155@gmail.com>
2019-05-30 05:48:39 -0400

    Added Page, Connection, PageManager, and ScrapeConfig

---
+++ src/main/java/com/div/diff/scraper/domain/Page.java
@@ -0,0 +1,34 @@
+package com.div.diff.scraper.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PAGES")
+public class Page {
+	
+	private String pageName;
+	private String url;
+	private String domain;
+	
+	@Column(name = "PAGE_NAME")
+	public String getPageName() {
+		return pageName;
+	}
+	public void setPageName(String pageName) {
+		this.pageName = pageName;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getDomain() {
+		return domain;
+	}
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+}
---
+++ src/main/java/com/div/diff/scraper/utils/PageManager.java
@@ -0,0 +1,39 @@
+package com.div.diff.scraper.utils;
+
+import java.util.Set;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class PageManager {
+
+	private SessionFactory factory;
+	
+	public void setup() {
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+		        .configure() // configures settings from hibernate.cfg.xml
+		        .build();
+		try {
+			factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		} catch (Exception ex) {
+		    StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+	
+	public Set<String> findNewPages(Set<String> pages) {
+		Session session = factory.openSession();
+		session.beginTransaction();
+		try {
+			
+		} finally {
+		session.get
+			session.getTransaction().commit();
+			session.close();
+		}
+		return null;
+	}
+	
+}
