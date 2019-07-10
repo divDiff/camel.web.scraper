58f1e66c3d9e8a676d51c84066b237b67fe2e45b
divdiff <mmaryon155@gmail.com>
2019-07-09 20:54:45 -0400

    Bumped up thread sleep to around 10 mins

--- src/test/java/com/mike/maryon/camel/web/scrapper/ScrapingRouteTest.java
+++ src/test/java/com/mike/maryon/camel/web/scrapper/ScrapingRouteTest.java
@@ -23,7 +23,7 @@ public class ScrapingRouteTest extends CamelTestSupport {
 				"23454:joe:30MAR2019\n";
 		MockEndpoint mock = getMockEndpoint("mock:output");
 		mock.expectedBodiesReceived(expected);
-		Thread.sleep(5000);
+		Thread.sleep(1000000000);
 		
 		File file = new File("data/output");
 		assertTrue(file.isDirectory());
