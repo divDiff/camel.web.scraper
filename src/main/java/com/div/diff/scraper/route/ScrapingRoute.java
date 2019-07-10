035b54a92736f44e63635c4a02331758178e9acd
divdiff <mmaryon155@gmail.com>
2019-07-09 20:53:12 -0400

    Added https to url matcher and made small mods to scraping route

--- src/main/java/com/div/diff/scraper/route/ScrapingRoute.java
+++ src/main/java/com/div/diff/scraper/route/ScrapingRoute.java
@@ -1,6 +1,7 @@
 package com.div.diff.scraper.route;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.ShutdownRunningTask;
 import org.apache.camel.builder.RouteBuilder;
 
 import com.div.diff.scraper.processor.ScrapingProcessor;
@@ -13,11 +14,12 @@ public class ScrapingRoute extends RouteBuilder {
 		from("file:data/input?noop=true&antInclude=links.txt&delay=5000")
 			.log("Before Rest Call to " + url)
 			.log("File contents are ${body}")
+			.shutdownRunningTask(ShutdownRunningTask.CompleteAllTasks)
 			.setHeader(Exchange.HTTP_METHOD, simple("GET"))
 			.setHeader("url", simple(url))
 			.to("http://" + url).process(new ScrapingProcessor())
 			.log("After Rest Call")
-			.to("file:data/output")
-			.to("mock:output");
+			.to("file:data/output");
+//			.to("mock:output");
 	}
 }
